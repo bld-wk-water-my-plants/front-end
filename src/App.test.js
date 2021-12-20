056e45ae79de073b4a00f21b52c30947a2a1f055
Anthony Coman <acoman@yahoo.com>
2021-12-20 12:28:47 -0600

    component files creation, and react router dom set to version 5

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Water my plants/i);
   expect(linkElement).toBeInTheDocument();
 });
