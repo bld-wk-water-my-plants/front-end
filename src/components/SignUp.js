2f604a2a76c9b3d459dc347f8b5d7ff803069325
Altora Mclean <altoramclean@gmail.com>
2021-12-21 00:01:58 -0500

    signup page attempt,nothing is showing on my side so i think i did it wrong

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,11 +1,86 @@
-import React from 'react'
+import React from "react";
 
-function Signup () {
-    return (
-        <div>
+/* this may need to be edited because i was rushing while trying to do this so im not confident in it*/
 
+
+const Signup = () => {
+  return (
+    <div className="top">
+      <div className="title">
+        <h2 className="signup">
+          Sign Up Here{" "}
+        </h2>
+      </div>
+
+      <div className="fill">
+          <form className="emailaddress" action="#" method="POST">
+            <div>
+              <label
+                htmlFor="email"
+              >
+                Email address
+              </label>
+              <div className="email1">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="style"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="styling"
+              >
+                Password
+              </label>
+              <div className="email1">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="style"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="styling"
+              >
+                Phone Number
+              </label>
+              <div className="email1">
+                <input
+                  id="phone"
+                  name="phone"
+                  type="phone"
+                  autoComplete="phone"
+                  required
+                  className="style"                />
+              </div>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className ="styling"
+              >
+                Submit Signup
+              </button>
+            </div>
+          </form>
         </div>
-    )
-}
+      </div>
+  );
+};
 
 export default Signup;
+
+
