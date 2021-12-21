c3b7fee6860a2605ba30f3c3c666469bdbd8f97f
Altora Mclean <altoramclean@gmail.com>
2021-12-20 23:58:32 -0500

    replaced package-lock json

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,85 +1,11 @@
-import React from "react";
+import React from 'react'
 
-const Signup = () => {
-  return (
-    <div className="top">
-      <div className="title">
-        <h2 className="signup">
-          Sign Up Here{" "}
-        </h2>
-      </div>
+function Signup () {
+    return (
+        <div>
 
-      <div className="fill">
-          <form className="emailaddress" action="#" method="POST">
-            <div>
-              <label
-                htmlFor="email"
-              >
-                Email address
-              </label>
-              <div className="email1">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="style"
-                />
-              </div>
-            </div>
-
-            <div>
-              <label
-                htmlFor="password"
-                className="styling"
-              >
-                Password
-              </label>
-              <div className="email1">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="style"
-                />
-              </div>
-            </div>
-            <div>
-              <label
-                htmlFor="email"
-                className="styling"
-              >
-                Phone Number
-              </label>
-              <div className="email1">
-                <input
-                  id="phone"
-                  name="phone"
-                  type="phone"
-                  autoComplete="phone"
-                  required
-                  className="style"                />
-              </div>
-            </div>
-            <div>
-              <button
-                type="submit"
-                className ="styling"
-              >
-                Submit Signup
-              </button>
-            </div>
-          </form>
         </div>
-      </div>
-  );
-};
+    )
+}
 
 export default Signup;
-
-
-
-
